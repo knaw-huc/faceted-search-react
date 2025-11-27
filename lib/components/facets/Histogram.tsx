cacbd97c1c2bc65b678b6816d97c3cf1f919ed0c
Jarno Bakker <jarno.bakker@di.huc.knaw.nl>
2025-11-27 13:45:32 +0100

    cleanup

--- lib/components/facets/Histogram.tsx
+++ lib/components/facets/Histogram.tsx
@@ -1,22 +1,16 @@
 import {FilterFacetItem} from "components/facets/FilterFacet.tsx";
 import * as d3 from "d3";
 
-export function Histogram({items, min, max, interval}: {items: FilterFacetItem[], min: number, max: number, interval: number}) {
+export function Histogram({items}: {items: FilterFacetItem[]}) {
     items.sort((a, b) => {
         if (a.itemKey > b.itemKey) return 1;
         if (b.itemKey > a.itemKey) return -1;
         return 0;
     })
 
-    console.log("items", items)
-
     const data_years = items.map((item) => Number(item.itemKey));
     const data_amounts = items.map((item) => item.amount);
     const data = items.map((item) => ({x: Number(item.itemKey), y: item.amount}))
-
-    // const all_years = Array.from({length: max - min + 1}, (_, i) => i + min);
-
-    console.log("data", data);
 
     const width = 300;
     const height = 150;
@@ -37,9 +31,7 @@ export function Histogram({items, min, max, interval}: {items: FilterFacetItem[]
                         <rect x={x(d.x)} y={marginTop} width={x.bandwidth()} height={height - marginBottom - marginTop} fill={"white"} stroke={"none"} opacity={0} />
                         <rect className={"barchart-bar-fill"} x={x(d.x)} y={y(d.y)} width={x.bandwidth()} height={height - 4 - y(d.y)} opacity={0.8} />
                         <text
-                            // x={x(d.x) + x.bandwidth() / 2}
                             x={marginLeft}
-                            // y={y(d.y) - 10}
                             y={marginTop / 2}
                             textAnchor={"start"}
                             alignmentBaseline={"central"}
@@ -53,5 +45,4 @@ export function Histogram({items, min, max, interval}: {items: FilterFacetItem[]
             </g>
         </svg>
     </>
-
 }
--- lib/components/hookedfacets/index.ts
+++ lib/components/hookedfacets/index.ts
@@ -1,4 +1,3 @@
 export {default as HookedFilterFacet, HookedFilterFacetItems} from './HookedFilterFacet';
 export {default as HookedRangeFacet} from './HookedRangeFacet';
 export {default as HookedSearchFacet} from './HookedSearchFacet';
-export {default as HookedHistogram} from './HookedHistogram';
