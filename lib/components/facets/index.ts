2ebbdb6550783653d8d6b26a75d57cf30c34e1f7
Jarno Bakker <jarno.bakker@di.huc.knaw.nl>
2025-11-27 13:45:32 +0100

    cleanup

--- lib/components/facets/index.ts
+++ lib/components/facets/index.ts
@@ -3,5 +3,6 @@ export {default as FacetsSection} from './FacetsSection';
 export {default as FilterFacet, FilterFacetItems} from './FilterFacet';
 export {default as RangeFacet} from './RangeFacet';
 export {default as SearchFacet} from './SearchFacet';
+export {Histogram} from './Histogram';
 
 export type {FilterFacetItem, Sort} from './FilterFacet';
--- lib/components/hookedfacets/HookedHistogram.tsx
+++
@@ -1,8 +0,0 @@
-import {FilterFacetItem} from "components/facets";
-import {Histogram} from "components/facets/Histogram.tsx";
-
-export default function HookedHistogram({items, min, max, interval}: {items: FilterFacetItem[], min: number, max: number, interval: number}) {
-    return <>
-        <Histogram items={items} min={min} max={max} interval={interval} />
-    </>
-}
