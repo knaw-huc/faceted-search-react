c97b3259a0fe4da7728d98f69eeb80cc0b5e0bdd
kerim1 <kerim.meijer@di.huc.knaw.nl>
2026-02-26 11:55:28 +0100

    Some histogram composition changes

--- lib/components/facets/range/Histogram.tsx
+++ lib/components/facets/range/Histogram.tsx
@@ -3,63 +3,88 @@ import {extent} from 'd3-array';
 import classes from './Histogram.module.css';
 import {useRef, useState} from "react";
 
+const width = 300;
+const height = 150;
+
+const marginLeft = 0;
+const marginRight = 0;
+const marginTop = 32;
+const marginBottom = 8;
+
 export interface HistogramItem {
     year: number | string;
     amount: number;
 }
 
-export default function Histogram({items}: {items: HistogramItem[]}) {
-    const [tooltipData, setTooltipData] = useState({
+interface TooltipData {
+    x: number;
+    y: number;
+    label: string;
+    visible: boolean;
+}
+
+export default function Histogram({items}: { items: HistogramItem[] }) {
+    const [tooltipData, setTooltipData] = useState<TooltipData>({
         label: "",
         x: 0,
         y: 0,
         visible: false,
     });
 
+    return (
+        <div className="mx-3">
+            <HistogramVisualization items={items} setTooltipData={setTooltipData}/>
+            <Tooltip {...tooltipData}/>
+        </div>
+    );
+}
+
+function HistogramVisualization({items, setTooltipData}: {
+    items: HistogramItem[],
+    setTooltipData: (value: TooltipData | ((prev: TooltipData) => TooltipData)) => void
+}) {
     const svgRef = useRef<SVGSVGElement>(null);
 
     const data_years = items.map((item) => item.year);
     const data_amounts = items.map((item) => item.amount);
     const data = items.map((item) => ({x: item.year, y: item.amount}))
 
-    const width = 300;
-    const height = 150;
-
-    const marginLeft = 0;
-    const marginRight = 0;
-    const marginTop = 32;
-    const marginBottom = 8;
-
     const x = scaleBand(data_years, [marginLeft, width - marginRight]).padding(0);
     const y = scaleLinear(extent(data_amounts) as [number, number], [height - marginBottom, marginTop]);
 
-    return <>
-        <div className={"mx-2 pb-2"}>
-            <svg onMouseLeave={() => setTooltipData({...tooltipData, visible: false})} ref={svgRef} width={"100%"} height={height} viewBox={"0 0 " + width + " " + height}>
-                <g fill={"var(--color-support-001)"} stroke={"currentColor"} strokeWidth={"1"}>
-                    {data.map((d, i) => (
-                        <g onMouseEnter={() => {
-                            const bounding = svgRef.current!.getBoundingClientRect();
-                            const tx = (x(d.x) as number) + bounding.left;
-                            const ty = y(d.y) + bounding.top - 50;
-                            setTooltipData({
-                                x: tx,
-                                y: ty,
-                                visible: true,
-                                label: d.x.toString() + ": " + d.y
-                        })}} className={classes['barchart-bar']} key={i}>
-                            <rect className={classes['barchart-bar-background']} x={Math.floor(x(d.x) as number)} y={marginTop} width={Math.ceil(x.bandwidth())} height={height - marginTop} stroke={"none"} opacity={1} />
-                            <rect className={classes['barchart-bar-fill']} x={Math.floor(x(d.x) as number)} y={Math.round(y(d.y))} width={Math.ceil(x.bandwidth())} height={Math.round(height - y(d.y))} opacity={0.8} strokeOpacity={0} />
-                        </g>
-                    ))}
-                </g>
-            </svg>
-            <div hidden={!tooltipData.visible} className={classes.tooltip} style={{
-                top: tooltipData.y,
-                left: tooltipData.x,
-            }}>
-                {tooltipData.label}
-            </div>
+    return (
+        <svg onMouseLeave={() => setTooltipData(tooltipData => ({...tooltipData, visible: false}))} ref={svgRef}
+             width={"100%"} height={height} viewBox={"0 0 " + width + " " + height}>
+            <g fill={"var(--color-support-001)"} stroke={"currentColor"} strokeWidth={"1"}>
+                {data.map((d) => (
+                    <g onMouseEnter={() => {
+                        const bounding = svgRef.current!.getBoundingClientRect();
+                        const tx = (x(d.x) as number) + bounding.left;
+                        const ty = y(d.y) + bounding.top - 50;
+                        setTooltipData({
+                            x: tx,
+                            y: ty,
+                            visible: true,
+                            label: d.x.toString() + ": " + d.y
+                        })
+                    }} className={classes['barchart-bar']} key={`${d.x}-${d.y}`}>
+                        <rect className={classes['barchart-bar-background']} x={Math.floor(x(d.x) as number)}
+                              y={marginTop} width={Math.ceil(x.bandwidth())} height={height - marginTop}
+                              stroke={"none"} opacity={1}/>
+                        <rect className={classes['barchart-bar-fill']} x={Math.floor(x(d.x) as number)}
+                              y={Math.round(y(d.y))} width={Math.ceil(x.bandwidth())}
+                              height={Math.round(height - y(d.y))} opacity={0.8} strokeOpacity={0}/>
+                    </g>
+                ))}
+            </g>
+        </svg>
+    );
+}
+
+function Tooltip({label, visible, x, y}: TooltipData) {
+    return (
+        <div hidden={!visible} className={classes.tooltip} style={{top: y, left: x}}>
+            {label}
         </div>
-    </>
+    );
 }
